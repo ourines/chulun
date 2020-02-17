74e00ab66fd0c96c59ad325bb548531de8a03a45
ourines <ourines@icloud.com>
2020-02-17 16:27:35 +0800

    init

---
+++ src/components/Menu.vue
@@ -0,0 +1,51 @@
+<template>
+  <el-row>
+    <el-col
+      class="title"
+      :span="4"
+    >
+      <h3 class="title">昆明楚轮商贸有限公司</h3>
+    </el-col>
+    <el-col :span="20">
+      <el-menu
+        :default-active="activeIndex2"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#f6f6e9"
+        active-text-color="#ffd04b"
+        router
+      >
+        <el-menu-item index="/">产品中心</el-menu-item>
+        <el-menu-item index="/about">关于公司</el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: "1",
+      activeIndex2: "1"
+    };
+  },
+  methods: {
+  }
+};
+</script>
+<style scoped>
+.title {
+  height: 60px;
+  font-size: 20px;
+
+  background-color: #545c64;
+  text-align: center;
+  line-height: 60px;
+  color: azure;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row>
+      <el-col :span="10">
+        <el-card>
+
+          <el-carousel trigger="click">
+            <el-carousel-item>
+              <img
+                src="../assets/1.jpg"
+                alt="1"
+              >
+            </el-carousel-item>
+            <el-carousel-item>
+              <img
+                src="../assets/2.jpg"
+                alt="2"
+              >
+            </el-carousel-item>
+            <el-carousel-item>
+              <img
+                src="../assets/3.jpg"
+                alt="3"
+              >
+            </el-carousel-item>
+          </el-carousel>
+        </el-card>
+
+      </el-col>
+      <el-col :span="14">
+        <el-card class="card">
+          <img
+            src="../assets/xcx.jpg"
+            alt=""
+          >
+          <img
+            src="../assets/ewm.jpg"
+            alt=""
+          >
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {}
+};
 </script>
+<style  scoped>
+.carousel_image_type {
+  width: 100%;
+}
+.card {
+  height: 50%;
+}
+</style>
